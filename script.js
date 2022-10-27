c9b4d9fde819b426a483bc426d8df3581c9dc24a
Rey  Baterbonia <poochinng@gmail.com>
2022-10-27 22:32:25 +0800

    init

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const toggleButton = document.getElementsByClassName('menu')[0] 
-const navbarLinks = document.getElementsByClassName('navbar-links')[0] 
-const body = document.getElementsByClassName('body')[0] 
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const body = document.body;
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     toggleButton.classList.toggle('active')
-    body.classList.toggle('active')
+    body.classList.toggle('body')
 })
